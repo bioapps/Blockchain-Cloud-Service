7e6573d4728096bb15dceb10c344723de5cf1693
Hektor Wallin <hektorw@gmail.com>
2016-02-09 13:15:26 +0100

    Copied all cool stuff from old repo

---
+++ crypto/public-key.js
@@ -0,0 +1,11 @@
+
+module.exports =
+	'-----BEGIN PUBLIC KEY-----\n' +
+	'MFwwDQYJKoZIhvcNAQEBBQADSwAwSAJBALAuAVkF+BpQtsivA/Cdwn64xEDQxuHB\n' +
+	'6zdB5/EVT4B2zeqZu4XO3zX+Ua2M641hGjqG0pcuovraVJLrFu0MFLMCAwEAAQ==\n' +
+	'-----END PUBLIC KEY-----';
+
+// module.exports = 
+// 	'-----BEGIN PUBLIC KEY-----\n' +
+// 	'MCswDQYJKoZIhvcNAQEBBQADGgAwFwIQcwSzaq166GMNv4fTOk5WUQIDAQAB\n' +
+// 	'-----END PUBLIC KEY-----\n';
